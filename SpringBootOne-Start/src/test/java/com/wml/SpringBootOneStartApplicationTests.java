ea92dd4a3dff6cb9fcfa2c4fbe6a5cdbb0a08031
NorwayWang <48284053+NorwayWang@users.noreply.github.com>
2020-01-22 15:35:14 +0800

    Create SpringBootOneStartApplicationTests.java

---
+++ SpringBootOne-Start/src/test/java/com/wml/SpringBootOneStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOneStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
