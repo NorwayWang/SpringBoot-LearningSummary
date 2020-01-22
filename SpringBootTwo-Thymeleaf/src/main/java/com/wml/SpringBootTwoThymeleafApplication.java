069e9eda6cd357ea038f4792d44fc2df0fb59c98
NorwayWang <48284053+NorwayWang@users.noreply.github.com>
2020-01-22 17:28:32 +0800

    Create SpringBootTwoThymeleafApplication.java

---
+++ SpringBootTwo-Thymeleaf/src/main/java/com/wml/SpringBootTwoThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.wml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwoThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwoThymeleafApplication.class, args);
+	}
+
+}
